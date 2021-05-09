09a0b90a2197f2fd99c347d350e89ff504c2bbea
Majid Bashir <majidbashir@icloud.com>
2021-05-09 18:25:38 +0500

    Initial Push after xCode setup
    Created Repo and setup xcode for the project.

---
+++ ProgressIndicator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProgressIndicator
+//
+//  Created by Majid Bashir on 09/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
